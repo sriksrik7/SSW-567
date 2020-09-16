3fd0b15892f1b76c4ac8802f3a6a8b4da60c9766
sriksrik7 <sriksrik7@gmail.com>
2020-09-16 11:24:24 -0400

    Part 2: HW02a
    Part 2:  Fixed the program classifyTriangle(). Updated the manual testreport file: HW02a.docx

--- HW02a/Triangle.py
+++ HW02a/Triangle.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan 14 13:44:00 2016
-Updated Jan 21, 2018
+Updated Sep 20, 2020
 
 The primary goal of this file is to demonstrate a simple python program to classify triangles
 
 @author: jrr
 @author: rk
+@author: Srikanth Uppada
 """
 
 def classifyTriangle(a,b,c):
@@ -27,31 +28,31 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
-    # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
-        return 'InvalidInput'
-    
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
         return 'InvalidInput';
+
+    # require that the input values be >= 0 and <= 200
+    if a > 200 or b > 200 or c > 200:
+        return 'InvalidInput'
+        
+    if a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
       
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if ((a + b) <= c) or ((b + c) <= a) or ((a + c) <= b):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
+    if a == b and a == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif ((a**2 + b**2) == c**2) or ((a**2 + c**2) == b**2) or ((b**2 + c**2) == a**2):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and  (b != c) and (c != a):
         return 'Scalene'
     else:
         return 'Isoceles'
